e9dcae167fbbf02a01bf64572170a28845747ccf
carwyndoan <ddoan@miu.edu>
2020-10-20 17:18:44 -0500

    Initial data

--- LibraryManagement/src/main/java/model/DataFactory.java
+++ LibraryManagement/src/main/java/model/DataFactory.java
@@ -17,25 +17,25 @@ public class DataFactory {
     }
 
     private static void initBooksData (){
-//        for (int i = 1; i <= 100; i++)
-//        {
-//            String strID 	= String.format("%03d", i);
-//            String strTitle = String.format("Title %03", i);
-//            String strAuthor = String.format("Author %03d", i);
-//            String strSubject = String.format("Subject %03d", i);
-//            String strIsbn = String.format("ISBN%03d", i);
-//            books.add(new Book(strID, strTitle, strAuthor, strSubject, strIsbn));
-//        }
+        for (int i = 1; i <= 100; i++)
+        {
+            String strID 	= String.format("%03d", i);
+            String strTitle = String.format("Title %03", i);
+            String strAuthor = String.format("Author %03d", i);
+            String strSubject = String.format("Subject %03d", i);
+            String strIsbn = String.format("ISBN%03d", i);
+            instance.getBookList().add(new Book(strID, strTitle, strAuthor, strSubject, strIsbn));
+        }
     }
 
     private static void initMembersData (){
-//        for (int i = 1; i <= 100; i++)
-//        {
-//            String strID 	= String.format("%03d", i);
-//            String strName = String.format("Name %03", i);
-//            String strAddress = String.format("Address %03d", i);
-//            String strPhone = String.format("Phone %03d", i);
-//            members.add(new Member(strID, strName, strAddress, strPhone));
-//        }
+        for (int i = 1; i <= 100; i++)
+        {
+            String strID 	= String.format("%03d", i);
+            String strName = String.format("Name %03", i);
+            String strAddress = String.format("Address %03d", i);
+            String strPhone = String.format("Phone %03d", i);
+            instance.getMemberList().add(new Member(strID, strName, strAddress, strPhone));
+        }
     }
 }
