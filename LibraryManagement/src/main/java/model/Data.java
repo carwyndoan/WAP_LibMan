f02b0c22167aaaa7b450f61d3cd9796fa35d27a5
hieule247 <54555669+hieule247@users.noreply.github.com>
2020-10-20 15:40:04 -0500

    update-booklist

--- LibraryManagement/src/main/java/model/Data.java
+++ LibraryManagement/src/main/java/model/Data.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Data {
     private List<Book> books;
 
+    // ------------------- Book Management
     public void loadDefaultData (){
         for (int i = 1; i <= 100; i++)
         {
@@ -17,10 +18,49 @@ public class Data {
         }
     }
 
-    public List getBooks() {
+    public List getBookList(){
         return books;
     }
 
-    public void addBook() {
+    public Book getBook(String id){
+        for (int i = 0; i < books.size(); i++){
+            Book aBook = books.get(i);
+            if (id.equals(aBook.getId()))
+                books.get(i);
+        }
+        // Not Found
+        return null;
+    }
+
+    public int getBookIdx(String id){
+        for (int i = 0; i < books.size(); i++){
+            Book aBook = books.get(i);
+            if (id.equals(aBook.getId()))
+                return i;
+        }
+        // Not Found
+        return -1;
+    }
+
+    public void addBook(Book aBook){
+        books.add(aBook);
+    }
+
+    public void addBook(String id, String title, String author, String subject, String isbn){
+        books.add(new Book(id, title, author, subject, isbn));
+    }
+
+    public void updBook(Book curBook, Book newBook){
+        curBook.setId(newBook.getId());
+        curBook.setTitle(newBook.getTitle());
+        curBook.setAuthor(newBook.getAuthor());
+        curBook.setSubject(newBook.getSubject());
+        curBook.setIsbn(newBook.getIsbn());
+    }
+
+    public void delBook(String id){
+        int idx = getBookIdx(id);
+        if (idx != -1)
+            books.remove(idx);
     }
 }
