43661534187f186d7f3f090ceadd0e11aae082ea
hieule247 <54555669+hieule247@users.noreply.github.com>
2020-10-20 16:35:41 -0500

    update-members

--- LibraryManagement/src/main/java/model/Data.java
+++ LibraryManagement/src/main/java/model/Data.java
@@ -1,12 +1,19 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Data {
     private List<Book> books;
+    private ArrayList<Member> members;
+
+    public Data(){
+        books   = new ArrayList<Book>();
+        members = new ArrayList<Member>();
+    }
 
     // ------------------- Book Management
-    public void loadDefaultData (){
+    public void initBooksData (){
         for (int i = 1; i <= 100; i++)
         {
             String strID 	= String.format("%03d", i);
@@ -63,4 +70,62 @@ public class Data {
         if (idx != -1)
             books.remove(idx);
     }
+
+    // ------------------- Member Management
+    public void initMembersData (){
+        for (int i = 1; i <= 100; i++)
+        {
+            String strID 	= String.format("%03d", i);
+            String strName = String.format("Name %03", i);
+            String strAddress = String.format("Address %03d", i);
+            String strPhone = String.format("Phone %03d", i);
+            members.add(new Member(strID, strName, strAddress, strPhone));
+        }
+    }
+
+    public List getMemberList(){
+        return members;
+    }
+
+    public Member getMember(String id){
+        for (int i = 0; i < members.size(); i++){
+            Member aMember = members.get(i);
+            if (id.equals(aMember.getId()))
+                members.get(i);
+        }
+        // Not Found
+        return null;
+    }
+
+    public int getMemberIdx(String id){
+        for (int i = 0; i < members.size(); i++){
+            Member aMember = members.get(i);
+            if (id.equals(aMember.getId()))
+                return i;
+        }
+        // Not Found
+        return -1;
+    }
+
+    public void addMember(Member aMember){
+        members.add(aMember);
+    }
+
+    public void addMember(String id, String name, String address, String phone){
+        members.add(new Member(id, name, address, phone));
+    }
+
+    public void updMember(Member curMem, Member newMem){
+        curMem.setId(newMem.getId());
+        curMem.setName(newMem.getName());
+        curMem.setAddress(newMem.getAddress());
+        curMem.setPhone(newMem.getPhone());
+    }
+
+    public void delMember(String id){
+        int idx = getMemberIdx(id);
+        if (idx != -1)
+            members.remove(idx);
+    }
+
 }
